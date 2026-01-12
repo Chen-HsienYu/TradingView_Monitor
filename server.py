2e01f4237e744b2f0ead932986c5aba2414dc17f
Chen-HsienYu <chenhsienyu0408@gmail.com>
2026-01-12 07:24:13 -0800

    Fix interval mapping logic

--- server.py
+++ server.py
@@ -103,5 +103,4 @@ def webhook():
 
 if __name__ == '__main__':
     # 根據 Railway 的環境變數設定 Port，本地端預設跑 5000
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
